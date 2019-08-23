2e3b37f98773473eec812d569df06eee95f4b89e
mrbarnk <mrbarnk1@gmail.com>
2019-08-23 18:52:03 +0100

    New Update

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 $(document).ready(function () {
-    $.get('products.json', (data) => {
+    $.get('https://raw.githubusercontent.com/mrbarnk/web-scrapping-with-javascript/master/products.json', (data) => {
         const json = JSON.parse(data)
-
-        console.log(json)
+        // const product = $("#product_div").clone()
+        json.forEach(item => {
+            const product = $("#product_div").clone()
+            product.removeAttr('hidden')
+            $("#product_title").html(item.title)
+            $("#product_image").attr('src', item.imageUrl)
+            $("#product_price").html(item.price)
+            product.appendTo("#product_parent")
+            console.log(item.price)
+        });
+        // console.log(json)
     })
 });
